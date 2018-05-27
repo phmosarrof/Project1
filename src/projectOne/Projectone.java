c4acddae43b62d9e5cca4763978e00319630f8f3
Mosarrof Hossain <phmosharaf@gmail.com>
2018-05-27 16:07:33 -0400

    Problem Solved

--- src/projectOne/Projectone.java
+++ src/projectOne/Projectone.java
@@ -4,7 +4,17 @@ public class Projectone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" problem solved");
+		
+		
+		System.out.println(" Problem solved");
+		
+		int x, y;
+		boolean z;
+		x = 10; 
+		y = 12;
+		z = x<y;
+		
+		System.out.println(" Z is "+(x<y));
 
 	}
 
